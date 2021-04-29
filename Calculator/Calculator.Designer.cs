5073af8b69fb0cf4c300671c68b57da6b0fa9929
Andron-debug <b.a.a.2002@yandex.ru>
2021-04-29 17:18:43 +0300

    Ввод 0, 1, ","

--- Calculator/Calculator.Designer.cs
+++ Calculator/Calculator.Designer.cs
@@ -33,6 +33,7 @@ namespace Calculator
             this.button2 = new System.Windows.Forms.Button();
             this.buttonFloat = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,6 +44,7 @@ namespace Calculator
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.num_click);
             // 
             // button2
             // 
@@ -52,6 +54,7 @@ namespace Calculator
             this.button2.TabIndex = 1;
             this.button2.Text = "0";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.num_click);
             // 
             // buttonFloat
             // 
@@ -61,6 +64,7 @@ namespace Calculator
             this.buttonFloat.TabIndex = 2;
             this.buttonFloat.Text = ",";
             this.buttonFloat.UseVisualStyleBackColor = true;
+            this.buttonFloat.Click += new System.EventHandler(this.num_click);
             // 
             // textBox1
             // 
@@ -71,11 +75,22 @@ namespace Calculator
             this.textBox1.Size = new System.Drawing.Size(776, 27);
             this.textBox1.TabIndex = 3;
             // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(705, 69);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(70, 70);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clear);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonFloat);
             this.Controls.Add(this.button2);
@@ -93,6 +108,7 @@ namespace Calculator
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button buttonFloat;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clear;
     }
 }
 
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -15,7 +15,34 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = num_str;
         }
 
+        string num_str = "0";
+        private void num_click(object sender, EventArgs e)
+        {
+            Button key = (Button)sender;
+            //if (num_str == "0") num_str = "";
+            switch (key.Text)
+            {
+                case "0":
+                    if (num_str != "0") num_str += "0";
+                        break;
+                case ",":
+                    if (!(num_str.Contains(','))) num_str += ",";
+                    break;
+                default:
+                    if (num_str == "0") num_str = "";
+                    num_str += key.Text;
+                    break;
+            }
+            textBox1.Text = num_str;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            num_str = "0";
+            textBox1.Text = num_str;
+        }
     }
 }
