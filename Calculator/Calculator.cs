2de74a6617734bc4313d14495d9cc99fe6eef5a7
Andron-debug <b.a.a.2002@yandex.ru>
2021-05-02 12:59:27 +0300

    Убрана переменная num_str

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -15,39 +15,36 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
-            textBox1.Text = num_str;
+            textBox1.Text = "0";
         }
 
-        string num_str = "0";
         private void num_click(object sender, EventArgs e)
         {
             Button key = (Button)sender;
             switch (key.Text)
             {
                 case "0":
-                    if (num_str != "0") num_str += "0";
+                    if (textBox1.Text != "0") textBox1.Text += "0";
                         break;
                 case ",":
-                    if (!(num_str.Contains(','))) num_str += ",";
+                    if (!(textBox1.Text.Contains(','))) textBox1.Text += ",";
                     break;
                 default:
-                    if (num_str == "0") num_str = "";
-                    num_str += key.Text;
+                    if (textBox1.Text == "0") textBox1.Text = "";
+                    textBox1.Text += key.Text;
                     break;
             }
-            textBox1.Text = num_str;
         }
 
         private void clear_Click(object sender, EventArgs e)
         {
-            num_str = "0";
-            textBox1.Text = num_str;
+            textBox1.Text = "0";
         }
 
         private void Parse_It_Click(object sender, EventArgs e)
         {
             double d;
-            if(Double.TryParse(num_str, out d))
+            if(Double.TryParse(textBox1.Text, out d))
             {
                 MessageBox.Show(d.ToString());
             }
@@ -55,9 +52,8 @@ namespace Calculator
 
         private void plus_minus_Click(object sender, EventArgs e)
         {
-            if ((num_str[0] != '0') && (num_str[0] != '-')) num_str = "-" + num_str;
-            else if (num_str[0] == '-') num_str = num_str.Substring(1);
-            textBox1.Text = num_str;
+            if ((textBox1.Text[0] != '0') && (textBox1.Text[0] != '-')) textBox1.Text = "-" + textBox1.Text;
+            else if (textBox1.Text[0] == '-') textBox1.Text = textBox1.Text.Substring(1);    
         }
     }
 }
